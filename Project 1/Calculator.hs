19ad2ad2605c3f52c27c2440ad6e7935e12c106d
João Ferreira <joaopedronf6b@gmail.com>
2025-12-27 12:51:47 +0000

    Made worksheet0 from Prolog

--- Project 1/Calculator.hs
+++ Project 1/Calculator.hs
@@ -5,21 +5,15 @@
 
   Pedro Vasconcelos, 2025
 -}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use <$>" #-}
 module Main where
 
 import Parsing
 import Data.Char
-import GHC.IO.Encoding (TextEncoding(textEncodingName))
 
 --
 -- a data type for expressions
 -- made up from integer numbers, + and *
 --
-type Name = String
-type Env = [(Name, Integer)]
-
 data Expr = Num Integer
           | Var String
           | Add Expr Expr
@@ -30,23 +24,23 @@ data Expr = Num Integer
           deriving Show
 
 data Command = Assign String Expr
-             | EvalExpr Expr
+             | Eval Expr
              deriving Show
 
+type Name = String
+type Env = [(Name, Integer)]
 -- a recursive evaluator for expressions
-
+--
 eval :: Env -> Expr -> Integer
-eval env (Num n)     = n
-eval env (Var x)     = case lookup x env of
-                         Just v  -> v
-                         Nothing -> error ("undefined variable: " ++ x)
-eval env (Add a b)   = eval env a + eval env b
-eval env (Sub a b)   = eval env a - eval env b
-eval env (Mul a b)   = eval env a * eval env b
-eval env (Div a b)   = eval env a `div` eval env b
-eval env (Rem a b)   = eval env a `rem` eval env b
-
-
+eval _ (Num n) = n
+eval env (Var x) = case lookup x env of
+                     Just b -> b
+                     Nothing -> error "undefinid variable"
+eval env (Add e1 e2) = eval env e1 + eval env e2
+eval env (Sub e1 e2) = eval env e1 - eval env e2
+eval env (Mul e1 e2) = eval env e1 * eval env e2
+eval env (Div e1 e2) = eval env e1 `div` eval env e2
+eval env (Rem e1 e2) = eval env e1 `mod` eval env e2
 
 -- | a parser for expressions
 -- Grammar rules:
@@ -67,10 +61,10 @@ exprCont :: Expr -> Parser Expr
 exprCont acc = do char '+'
                   t <- term
                   exprCont (Add acc t)
-            <|> do char '-'
-                   t <- term
-                   exprCont (Sub acc t)
-            <|> return acc
+              <|> do char '-'
+                     t <- term
+                     exprCont (Sub acc t)
+              <|> return acc
               
 term :: Parser Expr
 term = do f <- factor
@@ -80,40 +74,40 @@ termCont :: Expr -> Parser Expr
 termCont acc =  do char '*'
                    f <- factor  
                    termCont (Mul acc f)
-            <|> do char '/'
-                   f <- factor
-                   termCont (Div acc f)
-            <|> do char '%'
-                   f <- factor
-                   termCont (Rem acc f)    
-            <|> return acc
+                <|> do char '/'
+                       f <- factor
+                       termCont (Div acc f)
+                <|> do char '%'
+                       f <- factor
+                       termCont (Rem acc f)
+                <|> return acc
 
 factor :: Parser Expr
 factor = do n <- natural
             return (Num n)
-      <|> do char '('
+          <|>
+          do char '('
              e <- expr
              char ')'
              return e
-      <|> do v <- variable
-             return (Var v)       
+          <|> do v <- variable
+                 return (Var v)
              
-
-natural :: Parser Integer
-natural = do xs <- many1 (satisfy isDigit)
-             return (read xs)
-
-variable :: Parser String
-variable = do xs <- many1 (satisfy isAlpha)
-              return xs
-
 command :: Parser Command
 command = do v <- variable
              char '='
              e <- expr
              return (Assign v e)
-      <|> do e <- expr
-             return (EvalExpr e)
+          <|> do e <- expr
+                 return (Eval e)
+                 
+natural :: Parser Integer
+natural = do xs <- many1 (satisfy isDigit)
+             return (read xs)
+
+variable :: Parser String
+variable = do xs <- many1 (satisfy isLetter)
+              return xs
 
 ----------------------------------------------------------------             
   
@@ -125,18 +119,17 @@ main
 -- | read-eval-print loop
 calculator :: Env -> [String] -> IO ()
 calculator _ []  = return ()
-calculator env (l:ls) = do
-  let (out, env') = execute env l
-  putStrLn out
-  calculator env' ls
-
+calculator env (l:ls) = do 
+                    let (v, out) = execute env l
+                    putStrLn v
+                    calculator out ls
 
 execute :: Env -> String -> (String, Env)
 execute env txt
-  = case parse command txt of
-    [(Assign v e, "")] -> let val = eval env e
-                              env' = (v, val) : env
-                          in (show val, env')
-    [(EvalExpr e, "")] -> let val = eval env e
-                          in (show val, env)
-    _ -> ("parse error; try again", env)
+  = case parse command txt of 
+      [(Assign v e, "")] -> let val = eval env e
+                                env' = (v, val) : env
+                            in (show val, env')
+      [(Eval e, "")] -> let val = eval env e
+                        in (show val, env)
+      _ -> ("parse error; try again", env)  
